784692a6150657e80a8cb58959551155c2722c11
lawrence254 <lawrenslava1@gmail.com>
2022-01-21 23:07:59 +0300

    Finist implemeting tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+// Code your solution in this file
+
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+let createFareMultiplier = (multiplier) => {
+    return function (fare) { return multiplier * fare }
+}
+
+let fareDoubler = createFareMultiplier(2)
+
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, driverFn) => {
+    return driverFn(drivers)
+}
